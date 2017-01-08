7147c5665d5b139bce3738efcf06a4b28c8b1484
AwakEVE <upheaval212@gmail.com>
2017-01-09 00:30:54 +0900

    add requestHandler

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var server = require('./server');
+var router = require('./router');
+var requestHandler = require("./requestHandler");
 
-server.start();
+var handle = {};
+handle['/'] = requestHandler.start;
+handle["/start"] = requestHandler.start;
+handle["/upload"] = requestHandler.upload;
+
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -25,10 +25,15 @@ exports.start = start;
  */
 
 var http = require('http');
+var url = require('url');
 
-function start() {
+function start(route, handle) {
 	function onRequest(req, res) {
-		console.log('Request received');
+		var pathname = url.parse(req.url).pathname;
+		console.log('Request for ' + pathname + " received");
+
+		route(handle, pathname);
+
 		res.writeHead(200, {'Content-Type' : 'text/plain'});
 		res.write('Hello World');
 		res.end();
